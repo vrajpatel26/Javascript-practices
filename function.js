6067b2009b9e2fc4fa9e8b3da23a4ac71b58740b
vrajpatel26 <vraj.26112004@gmail.com>
2024-12-16 20:00:29 +0530

    added other fun ex

--- function.js
+++ function.js
@@ -1,6 +1,60 @@
- function data(a,b){
-    console.log(`${a}+${b} = ${a+b}`);
-    console.log(`${a}*${b} = ${a*b}`);
-    
+//  function data(a,b){
+//     console.log(`${a}+${b} = ${a+b}`);
+//     console.log(`${a}*${b} = ${a*b}`);
+
+// }
+// data(2,3)
+
+
+//calculator
+
+/* const calculator = (num1, num2, operator) => {
+
+    switch (operator) {
+        case '+': console.log(`${num1}+${num2} = ${num1 + num2}`);
+            break;
+        case '-': console.log(`${num1}-${num2} = ${num1 - num2}`);
+            break;
+        case '*': console.log(`${num1}*${num2} = ${num1 * num2}`);
+            break;
+        case '/': console.log(`${num1}/${num2} = ${num1 / num2}`);
+            break;
+        default: console.log('error !!')
+    }
 }
-data(2,3)
+calculator(10, 0, '/') */
+
+
+//reverse string 
+
+/* const reverse = (str) =>{
+
+    let reverse = " "
+    for(let idx = str.length-1 ; idx >= 0  ; idx--){
+        
+        reverse = reverse + str[idx] 
+    }
+    return reverse;
+}
+console.log(reverse("vraj patel")); */
+
+
+
+//check palindrom or not 
+
+/* const palindrom = (str) => {
+
+    let reverse = ""
+    for (let idx = str.length - 1; idx >= 0; idx--) {
+
+        reverse = reverse + str[idx];
+    }
+    if (str === reverse) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+// console.log(palindrom("vraj patel"));
+console.log(palindrom("level")); */
