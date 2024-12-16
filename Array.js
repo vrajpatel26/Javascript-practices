0113b9837a14fc9265dcc82a101760fb9509adb5
vrajpatel26 <vraj.26112004@gmail.com>
2024-12-16 22:31:32 +0530

    add array.js

---
+++ Array.js
@@ -0,0 +1,26 @@
+/*const family = ["vraj","viraj","nidhi","mom","papa"]
+
+console.log(family[-1]);//error 
+
+console.log(family.at(-1)); //papa
+console.log(family.at(-2)); //mom
+*/
+
+
+//For-of loop - print all data 
+
+/*const Fruits = ["apple","Banana","graps","dragon","orange"]
+
+for(let item of Fruits){
+    console.log(item);
+}*/
+
+
+//for-in loop - show index number of array (in stands for index)
+
+
+const fruits = ["apple","Banana","graps","dragon","orange"]
+
+for(let item in fruits){
+    console.log(item);
+}
