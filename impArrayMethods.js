e078eb982c8fc8061672c211edbd6477695c40fd
vrajpatel26 <vraj.26112004@gmail.com>
2024-12-18 13:10:56 +0530

    add

---
+++ impArrayMethods.js
@@ -0,0 +1,33 @@
+const friends = ["vraj","viraj","radhe","hardik","parv"]
+
+// --------indexOf - give the index number of element ---------
+
+/*
+console.log(friends.indexOf("hardik")); //3
+
+console.log(friends.indexOf("hardik",6)); // -1 bcz it check idx value 6 and go next but the hardik is placed befor idx 6.
+*/
+
+//lastIndexOf()-return last index in case of same value
+
+/*
+const fruits = ["graps","banana","apple","graps","orange"]
+//here graps are two times
+
+console.log(fruits.indexOf("graps")); //0
+
+console.log(fruits.lastIndexOf("graps"));//3 - return last index of graps
+*/
+
+
+//---------include method - return true/false - element available or not
+
+/*
+console.log(friends.includes("hardik")); //true
+
+console.log(friends.includes("henil")); //false
+*/
+
+
+
+
