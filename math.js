8755d89b260fb724800532ca8abf8a48961e9513
vrajpatel26 <vraj.26112004@gmail.com>
2025-01-03 21:34:15 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,73 @@
+
+/*
+const piValue = Math.PI ;
+console.log(piValue); //3.141592653589793
+*/
+
+//---- abs() - return nuber that indiacate how far the number is from 0.
+
+/*
+console.log(Math.abs(5));//5
+console.log(Math.abs(-3));//3
+*/
+
+// ------- Math.round() - rounds a number to the nearest intiger.
+
+/*
+const number = Math.round(3.4)
+console.log(number); //3
+
+const number2 = Math.round(3.5)
+console.log(number2); //4
+
+const number3 = Math.round(3.7)
+console.log(number3); //4
+*/
+
+
+
+
+//---------Math.ceil() - return the +1 value of given number
+//undestand as ghar ki chattt
+
+/*
+const number = Math.ceil(3.7)
+console.log(number); //4
+
+const number3 = Math.ceil(4.2)
+console.log(number3); //5
+*/
+
+
+//-----------Math.floor() - return the value befor point(.)
+//understand as ghar ka floor 
+
+
+/*
+const number = Math.floor(3.7)
+console.log(number); //3
+
+const number2 =  Math.floor(4.01)
+console.log(number2) //4
+
+const number4 = Math.floor(-3.7)
+console.log(number4); //-4 
+*/
+
+
+// ------Math.trunc() - return same integer value 
+
+const number = Math.trunc(3.7)
+console.log(number); //3
+
+const number2 = Math.trunc(-3.7)
+console.log(number2); //-3
+
+//trunc() and floor are most of same but difference is :- 
+// floor gives smallest value
+
+const number3 = Math.trunc(-3.7)
+console.log(number3); //-3
+
+const number4 = Math.floor(-3.7)
+console.log(number4); //-4 
