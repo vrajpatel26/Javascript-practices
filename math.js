64660b7800ab499c3d014a93c1830de7585a3d82
vrajpatel26 <vraj.26112004@gmail.com>
2025-01-05 11:49:29 +0530

    Update math.js

--- math.js
+++ math.js
@@ -56,7 +56,7 @@ console.log(number4); //-4
 
 
 // ------Math.trunc() - return same integer value 
-
+/*
 const number = Math.trunc(3.7)
 console.log(number); //3
 
@@ -71,3 +71,58 @@ console.log(number3); //-3
 
 const number4 = Math.floor(-3.7)
 console.log(number4); //-4 
+*/
+
+
+
+
+
+//---------Math.pow(x,y) ;- return the value of x to the power of y.
+
+/*
+const number = Math.pow(2,3);
+console.log(number); //8
+
+//nowdays we use this method 
+console.log(2**3); //8
+*/
+
+
+
+
+//-------Math.sqrt() - return the square root of x
+
+
+//console.log(Math.sqrt(25)); //5
+
+
+
+//-----------Math.log(x) - return the logarithm of x.
+
+/*
+console.log(Math.log(100)); //4.6051
+
+console.log(Math.log2(100)); //6.6438
+
+
+console.log(Math.log10(100)); //2
+*/
+
+
+//----------Math.random()- return the random value between 0(include) & 1(exclusive).
+
+console.log(Math.random());
+
+console.log(Math.random() * 100); // 0 to 100
+
+console.log(Math.floor(Math.random() * 100)); // 0-100 wihtout decimal
+
+console.log((Math.random()*100).toFixed(2)); //0-100 with 2 decimal value like 44.99
+
+
+
+
+
+
+
+
