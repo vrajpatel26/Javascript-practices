5b572ad4a72177966b9ca255db60f2897a91786e
vrajpatel26 <vraj.26112004@gmail.com>
2025-03-09 12:59:29 +0530

    adding into js

---
+++ Localstorage.js
@@ -0,0 +1,19 @@
+// set data in local storage
+
+localStorage.setItem("name","Asus");
+localStorage.setItem("model","F15");
+localStorage.setItem("color","black");
+
+
+// get data from localstorage
+
+localStorage.getItem("model") //f15
+
+// remove data from localstorage
+
+localStorage.removeItem("color") //remove this data
+
+
+
+//---------------------------JSON--------------------------------
+
