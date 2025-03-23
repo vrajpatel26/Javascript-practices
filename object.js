60add50184c59710f622b734bd2a1848967ad8b0
vrajpatel26 <vraj.26112004@gmail.com>
2025-03-23 11:31:29 +0530

    Update object.js

--- object.js
+++ object.js
@@ -19,3 +19,31 @@ person.greet() //i am function
 // console.log(person."is'student"); // X-wrong
 
 console.log(person["is'student"]);
+
+// -------------------------------------------------------------
+
+idType = "studentId"
+const student = {
+    idType : "22BECE354",
+    age : 20,
+    sem : 6,
+    course : "CE",
+    greet: function(){
+        console.log(`Hey , My ${idType} is ${student.idType} and my course is ${student.course} `);
+       
+    }
+}
+student.greet() //Hey , My studentId is 22BECE354 and my course is CE
+
+student.Favsubject = "Maths";
+console.log(student);
+/*{
+    idType: '22BECE354',
+    age: 20,
+    sem: 6,
+    course: 'CE',
+    greet: [Function: greet],
+    Favsubject: 'Maths'
+}*/
+
+
